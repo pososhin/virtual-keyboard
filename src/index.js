76b882aa75677ca3ae491328da03f0a91b3b81bb
Посошин Дмитрий <C:\Users\dp\AppData\Roaming\The Bat!>
2022-05-10 02:45:28 +0300

    feat: h1

--- src/index.js
+++ src/index.js
@@ -12,7 +12,20 @@ root.append(debug);
 const container = document.createElement("div");
 container.classList.add("container");
 root.append(container);
+const h1 = document.createElement("h1");
+h1.textContent = 'RSS Virtual Keyboard'
+
+const note1 = document.createElement("p");
+note1.textContent = 'Keyboard made for Windows 10'
+const note2 = document.createElement("p");
+note2.textContent = 'Language switching Shift - Alt'
+
+
+
+container.append(h1);
 container.append(terminal.element);
 container.append(keyboard.element);
+container.append(note1);
+container.append(note2);
 
 
